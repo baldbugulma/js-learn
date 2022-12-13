f5e4250c0fa9ed4e7965fe6d74a83c4bcce73c8e
Konstantin <ForZaArindal@gmail.com>
2022-12-13 13:57:14 +0300

    Операторы присваивания

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+// Операторы присваивания
+
+let age = 18+5;
+
+age+=2; // age = age+2
+age++; //age=age+1
+console.log(age);
+
+//Операторы сравнения
+
+const vasya = 20;
+console.log(age > vasya)
+console.log(age >= vasya)
+console.log(age <= vasya)
+console.log(age == vasya)
