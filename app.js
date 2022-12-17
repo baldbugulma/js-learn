6f6df619e8f9e37a0b727bec0410f78a11255aa2
Konstantin <ForZaArindal@gmail.com>
2022-12-17 13:33:18 +0300

    Switch

--- app.js
+++ app.js
@@ -2,20 +2,43 @@
 // Васи положил 12 000$ на вклад 7% годовых с капитализацией 1 раз в месяц. Вывести в консоль, сможет ли он купить дом за 13 500$ через 2 года после снятия вклада. И остаток после покупки.
 // Итог = Сумма * (1 + Ставка в месяц не в %) ^ срок в месяцах
 
-let deposit = 12000;
-let percentYear = 0.07;
-let depositTerm = 24;
+const role = 'CEO';
 
-let total = deposit*(1 + percentYear/12)**24;
-let accountBalance = total - 13500;
-
-const template = `Может купить дом за 13500$
-Останется после покупки ${accountBalance}$`;
-const template2 = `Не может купить дом`;
-
-if (total >= 13500){
-    console.log(template);
-} else {
-    console.log(template2)
+switch(role){
+    case 'manager':
+        console.log('Менеджер');
+        break;
+    case 'admin':
+        console.log('Админ');
+        break;
+    case 'CEO':
+        console.log('CEО');
+        break;
+    default:
+        console.log('Мы тебя не знаем'); 
 }
 
+switch(role){
+    case 'manager':
+    case 'admin':
+        console.log('Не руководитель');
+        break;
+    case 'CEO':
+        console.log('CEО');
+        break;
+    default:
+        console.log('Мы тебя не знаем'); 
+}
+
+const num = 1;
+
+switch(true){
+    case num>0: // true === num > 0
+        console.log ("Положительный")
+        break;
+    case num<0:
+        console.log ("Отрицательный")
+        break;
+    default:
+        console.log ("Ноль")
+}
