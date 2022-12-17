5d9d7d3fbc765356f2c407323bb5f137a7bda98a
Konstantin <ForZaArindal@gmail.com>
2022-12-17 13:05:45 +0300

    Задача - Размещение депозита

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-//Условные операторы
 
-const money = 1;
-const canBay = money > 50;
+// Васи положил 12 000$ на вклад 7% годовых с капитализацией 1 раз в месяц. Вывести в консоль, сможет ли он купить дом за 13 500$ через 2 года после снятия вклада. И остаток после покупки.
+// Итог = Сумма * (1 + Ставка в месяц не в %) ^ срок в месяцах
 
+let deposit = 12000;
+let percentYear = 0.07;
+let depositTerm = 24;
 
+let total = deposit*(1 + percentYear/12)**24;
+let accountBalance = total - 13500;
 
-if(canBay){
-    console.log('Может купить')
-} else if (money > 5){
-    console.log('Купи мини')
+const template = `Может купить дом за 13500$
+Останется после покупки ${accountBalance}$`;
+const template2 = `Не может купить дом`;
+
+if (total >= 13500){
+    console.log(template);
 } else {
-    console.log('Не может купить')
+    console.log(template2)
 }
-
 
