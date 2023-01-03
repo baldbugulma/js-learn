bcc8ee1a31b423e7e1b6072d96b7ced875156533
Konstantin <ForZaArindal@gmail.com>
2023-01-03 20:05:48 +0300

    Аргументы функции

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-const toPowerArrow = (num,power) => num*power;
-console.log(toPowerArrow(2,3))
+function toPower(num,power = 2){
+    const res = num**power;
+    return res;
+}
+
+console.log(toPower(2,3));
+console.log(toPower(2));
