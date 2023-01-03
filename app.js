59b768c98c3f89dbe4a72b81cb9578ebec15289c
Konstantin <ForZaArindal@gmail.com>
2023-01-03 19:56:23 +0300

    Стрелочные функции

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
-// Анонимные функции
-
-function powerOfTwo(num){
-    return num*num;
-}
-
-console.log(powerOfTwo(5))
+const toPowerArrow = (num,power) => num*power;
+console.log(toPowerArrow(2,3))
